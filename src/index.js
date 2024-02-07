ddaad027f1dd4beffaf227880e99ea265bf904e1
kyubiner <hamdaraksa@gmail.com>
2024-02-08 06:24:53 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const { Client, IntentsBitField } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ]
+});
+
+client.on('ready', (c) => {
+    console.log(`p ${c.user.tag} is online.`);
+})
+
+client.on('messageCreate', (message) => {
+    if (message.author.bot) {
+        return;
+    }
+    
+    if (message.content === 'hai') {
+        message.reply('hai juga bre');
+    }
+})
+
+client.login(process.env.TOKEN);
